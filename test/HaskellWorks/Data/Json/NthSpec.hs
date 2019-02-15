bfd79b2d137a178e0d003c2ad487673886a782eb
John Ky <newhoggy@gmail.com>
2019-02-16 10:05:19 +1100

    Update to hw-json-1.0.0.1

--- test/HaskellWorks/Data/Json/NthSpec.hs
+++ test/HaskellWorks/Data/Json/NthSpec.hs
@@ -3,29 +3,21 @@
 module HaskellWorks.Data.Json.NthSpec (spec) where
 
 import Control.Lens
-import Data.Word
 import GHC.Base
-import HaskellWorks.Data.BalancedParens.Simple
-import HaskellWorks.Data.Bits.BitShown
-import HaskellWorks.Data.FromByteString
+import HaskellWorks.Data.Json.Internal.PartialIndex
 import HaskellWorks.Data.Json.Lens
 import HaskellWorks.Data.Json.PartialValue
-import HaskellWorks.Data.Json.Succinct.Cursor.Internal
-import HaskellWorks.Data.Json.Succinct.PartialIndex
-import Prelude                                         hiding (null)
+import Prelude                                      hiding (null)
 import Test.Hspec
 
-import qualified Data.ByteString      as Strict
-import qualified Data.Vector.Storable as DVS
+import qualified Data.ByteString                              as Strict
+import qualified HaskellWorks.Data.Json.Backend.Standard.Slow as SLOW
 
 {-# ANN module ("HLint: ignore Redundant do"        :: String) #-}
 {-# ANN module ("HLint: ignore Reduce duplication"  :: String) #-}
 
-loadJson :: Strict.ByteString -> JsonCursor Strict.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64))
-loadJson = fromByteString
-
 j :: Strict.ByteString -> JsonPartialValue
-j = jsonPartialJsonValueAt . jsonPartialIndexAt . loadJson
+j = jsonPartialJsonValueAt . jsonPartialIndexAt . SLOW.makeCursor
 
 spec :: Spec
 spec = describe "Nth Spec" $ do
